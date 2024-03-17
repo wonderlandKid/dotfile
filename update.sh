dde6926c752336c8060b3618a3451dad21361235
dtfulffroot <2369416824@qq.com>
2024-03-17 19:24:45 +0800

    update

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
-trash alacritty yazi
+trash alacritty yazi rime/*
 cp ~/.config/{alacritty,yazi} . -r
-# cp /home/liu/.local/share/fcitx5/rime . -r
+cp ~/.local/share/fcitx5/rime/{clover.userdb,luna_pinyin.userdb} -r rime/
 
 trash hyprland/{dunst,hypr,rofi,waybar}
 cp ~/.config/{dunst,hypr,rofi,waybar} hyprland/ -r
