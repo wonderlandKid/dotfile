87e490b845465eafe44a01ac68d11e8f548a0697
dtfulffroot <2369416824@qq.com>
2024-03-17 17:25:12 +0800

    stop update rime

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
-trash alacritty yazi rime 
+trash alacritty yazi
 cp ~/.config/{alacritty,yazi} . -r
-cp /home/liu/.local/share/fcitx5/rime . -r
+# cp /home/liu/.local/share/fcitx5/rime . -r
 
 trash hyprland/{dunst,hypr,rofi,waybar}
 cp ~/.config/{dunst,hypr,rofi,waybar} hyprland/ -r
