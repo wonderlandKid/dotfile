4b31bfab114045190372192ad4f6eacfdab71726
liu <2369416824@qq.com>
2024-04-18 12:34:13 +0800

    update

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@ trash alacritty yazi rime/*
 cp ~/.config/{alacritty,yazi} . -r
 cp ~/.local/share/fcitx5/rime/{clover.userdb,luna_pinyin.userdb} -r rime/
 
-trash hyprland/{dunst,hypr,rofi,waybar}
-cp ~/.config/{dunst,hypr,rofi,waybar} hyprland/ -r
+# trash hyprland/{dunst,hypr,rofi,waybar}
+# cp ~/.config/{dunst,hypr,rofi,waybar} hyprland/ -r
